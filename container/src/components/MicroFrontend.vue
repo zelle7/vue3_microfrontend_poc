293d32664d49db00323e8a7329e4859dae28684f
Christian Zellot <christian.zellot@beenergised.com>
2022-08-04 01:06:17 +0200

    Setup

---
+++ container/src/components/MicroFrontend.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MicroFrontend"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ wisdom/src/components/RandomWisdom.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "RandomWisdom"
+}
+</script>
+
+<style scoped>
+
+</style>
